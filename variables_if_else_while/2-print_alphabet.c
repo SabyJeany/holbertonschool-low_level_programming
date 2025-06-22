22ba9c70bb93349bdec4edcf88157fb0c981fda2
SabyJeany <Sabyjeany@gmail.com>
2025-06-22 15:48:17 +0200

    2printalphabet

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,27 +1,22 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+
 /**
- * main - entry point
+ * main - Entry point of the program
  *
- * Return: 0 (if success)
+ * Description: Prints the alphabet in lowercase
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n < 0)
-{
-printf("%d is negative\n", n);
-}
-else if (n == 0)
-{
-printf("%d is zero\n", n);
-}
-else
-{
-printf("%d is positive\n", n);
-}
-return (0);
+	char letter = 'a';
+
+	/* Loop through the alphabet */
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n'); /* Print a new line */
+	return (0);
 }
