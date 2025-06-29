673c1f13f8a151faaf2c284570fab5b99430ee11
SabyJeany <Sabyjeany@gmail.com>
2025-06-30 00:42:35 +0200

    8printarray

---
+++ pointers_arrays_strings/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int array[] = {0, 1, 2, 3, 4, 98, 402, 1024};
+    print_array(array, 8);
+    return (0);
+}
