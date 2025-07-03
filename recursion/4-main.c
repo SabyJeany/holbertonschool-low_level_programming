05713de7cd6b68a54a243895589f884b3ed73545
SabyJeany <Sabyjeany@gmail.com>
2025-07-03 15:39:53 +0200

    4powrecursion

---
+++ recursion/4-main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d\n", _pow_recursion(2, 3));
+    printf("%d\n", _pow_recursion(5, 0));
+    printf("%d\n", _pow_recursion(2, -2));
+    return (0);
+}
